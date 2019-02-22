23b2aa90c1e0de491b998218fcfbef1e990a760e
peng.xing <southeast_px@163.com>
2019-02-22 16:27:06 +0800

    Shiro_从数据表中初始化资源和权限!

---
+++ src/cn/ksdshpx/shiro/factory/FilterChainDefinitionMapBuilder.java
@@ -0,0 +1,22 @@
+package cn.ksdshpx.shiro.factory;
+/**
+ * @author peng.x
+ * @date 2019年2月22日 下午4:02:02
+ */
+
+import java.util.LinkedHashMap;
+
+public class FilterChainDefinitionMapBuilder {
+	public LinkedHashMap<String, String> bulidFilterChainDefinitionMap(){
+		LinkedHashMap<String, String> map = new LinkedHashMap<>();
+		//从数据表中初始化资源和权限
+		map.put("/login.jsp", "anon");
+		map.put("/shiro/login", "anon");
+		map.put("/shiro/logout", "logout");
+		map.put("/admin.jsp", "roles[admin]");
+		map.put("/User.jsp", "roles[user]");
+		map.put("/**", "authc");
+		
+		return map;
+	}
+}
